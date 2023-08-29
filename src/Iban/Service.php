de4a6ec17e3557c9c674dc7becb2b0d1e1ad887f
Philip Falliano <philip.falliano@phillopp.de>
2023-08-29 14:50:31 +0200

    add facade

--- src/Iban/Service.php
+++ src/Iban/Service.php
@@ -6,7 +6,10 @@ use Iban\Validation\Iban as IbanObject;
 
 class Service
 {
-    public function format(string $ibanString, string $format = IbanObject::FORMAT_PRINT): string
+    const FORMAT_PRINT = IbanObject::FORMAT_PRINT;
+    const FORMAT_ELECTRONIC = IbanObject::FORMAT_ELECTRONIC;
+
+    public function format(string $ibanString, string $format = self::FORMAT_PRINT): string
     {
         $iban = new IbanObject($ibanString);
         return $iban->format($format);
