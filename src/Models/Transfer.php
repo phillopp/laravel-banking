1f7cbc0a3837f711d97985f73eb1d4971221f840
Philip Falliano <philip.falliano@phillopp.de>
2024-03-30 11:43:25 +0100

    add transfer test

--- src/Models/Transfer.php
+++ src/Models/Transfer.php
@@ -3,27 +3,25 @@
 namespace Phillopp\LaravelBanking\Models;
 
 use Illuminate\Support\Str;
-use Phillopp\LaravelBanking\Exception\PurposeCodeNotFound;
 use Phillopp\LaravelBanking\Exception\TransferException;
 use Phillopp\LaravelBanking\Sepa\PurposeCode;
-use Phillopp\LaravelBanking\Transfer\Service;
 
 class Transfer
 {
     private Bankaccount $recipient;
     private float $amount;
-    private string $purpose;
-    private string $reference;
-    private Bankaccount $sender;
-    private PurposeCode $purposeCode;
+    private ?string $purpose;
+    private ?string $reference;
+    private ?Bankaccount $sender;
+    private ?PurposeCode $purposeCode;
 
     public function __construct(
         Bankaccount $recipient,
         float       $amount,
-        string      $purpose = null,
-        string      $reference = null,
-        Bankaccount $sender = null,
-        PurposeCode $purposeCode = null
+        ?string      $purpose = null,
+        ?string      $reference = null,
+        ?Bankaccount $sender = null,
+        ?PurposeCode $purposeCode = null
     )
     {
         if (!empty($reference) && !empty($purpose)) {
@@ -48,12 +46,12 @@ class Transfer
         return $this->amount;
     }
 
-    public function getPurpose(): string
+    public function getPurpose(): ?string
     {
         return $this->purpose;
     }
 
-    public function getReference(): string
+    public function getReference(): ?string
     {
         return $this->reference;
     }
@@ -63,7 +61,7 @@ class Transfer
         return $this->sender;
     }
 
-    public function getPurposeCode(): PurposeCode
+    public function getPurposeCode(): ?PurposeCode
     {
         return $this->purposeCode;
     }
@@ -72,18 +70,16 @@ class Transfer
         string $userHint = ''
     ): string
     {
-        $epcDefault = "BCD\n002\n1\nSCT\n%s\n%s\n%s\nEUR%s\n%s\n%s\n%s\n%s";
-
         $purposeCode = '';
         if (!empty($this->getPurposeCode())) {
             $purposeCode = $this->getPurposeCode()->getCode();
         }
 
         return sprintf(
-            $epcDefault,
+            "BCD\n002\n1\nSCT\n%s\n%s\n%s\nEUR%s\n%s\n%s\n%s\n%s",
             $this->getRecipient()->getBic(),
             Str::substr($this->getRecipient()->getAccountHolder(), 0, 70),
-            $this->getRecipient()->getIban(),
+            $this->getRecipient()->getIban()->getNormalizedIban(),
             $this->getAmount(),
             $purposeCode,
             $this->getReference(),
